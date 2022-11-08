2c97700ca939f85d702754b168ebbd4bfbf8be85
Jared <jaredt2003@hotmail.com>
2022-11-08 02:55:20 -0500

    cleaned up safety program creator

--- safety_program_creator.py
+++ safety_program_creator.py
@@ -1,14 +1,11 @@
-from venv import create
 from docxtpl import DocxTemplate
 from docxtpl.subdoc import Subdoc, SubdocComposer
 from docx import Document
 from zipfile import ZipFile
 
-from lxml import etree  # TODO for debug
+from lxml import etree
 import re
 import io
-
-import types
 
 import pythoncom
 import win32com.client
@@ -16,38 +13,18 @@ import tempfile
 
 import os
 
-# import field_updater
-# UNO = field_updater.UNO(
-#     "127.0.0.1", "60000"
-# )
-
-# import win32com.client
-
-# import inspect, os
-
-
-def findPath(file_name):
-    script_dir = os.path.dirname(__file__)  # absolute dir the script is in
-    rel_path = f"Safety Programs/{file_name}"
-    abs_file_path = os.path.join(script_dir, rel_path)
-    return abs_file_path
-
-# def update_toc(docx_file):
-#     word = win32com.client.DispatchEx("Word.Application")
-#     doc = word.Documents.Open(docx_file)
-#     doc.TablesOfContents(1).Update()
-#     doc.Close(SaveChanges=True)
-#     word.Quit()
 
 class Tempdoc():
     def __init__(self, data: bytes, filetype: str = "docx", word=None):
+        self.temp_files = []
+
         if not word:
             word = self.word_instance()
-            self.word_given = False # need to close word if it wasnt given
-        else:
-            self.word_given = True
+            self.word_given = False
+        else:  # allow passing instance for purpose of batch operations
+            self.word_given = True  # need to not close word if it wasnt given
 
-        self.path = self.make_temp(data, filetype)
+        self.path = self.make_temp(data, filetype)  # path on disk
         self.doc = word.Documents.Open(self.path)
 
         self.word = word
@@ -58,12 +35,14 @@ class Tempdoc():
             "pdf": 17
         }
 
+    # Create msword instance
     @staticmethod
     def word_instance():
         pythoncom.CoInitialize()
         word = win32com.client.DispatchEx("Word.Application")
         return word
 
+    # Create a temporary file in windows temp folder.
     def make_temp(self, data, filetype):
         Counter = 0
         TEMPPATH = tempfile.gettempdir()
@@ -75,21 +54,27 @@ class Tempdoc():
                 break
             except:
                 Counter += 1
+
+            if Counter >= 100:
+                raise Exception
+
+        self.temp_files.append(path)  # add to list of files to be destroyed on exit
         return path
 
-    def save_as(self, _format="pdf"):
-        # Good idea to make an empty file and overwrite later
-        path = self.make_temp(b"", _format)
+    # save and return bytes
+    def save(self, _format=None):
+        if _format:
+            # Need to make a new temp file if we are going to save as a different format
+            path = self.make_temp(b"", _format)
 
-        self.doc.SaveAs(
-            path,
-            FileFormat=self.format[_format]
-        )
-        return self.read(path)
-
-    def save(self):
-        self.doc.Close(SaveChanges=True)
-        return self.read(self.path)
+            self.doc.SaveAs(
+                path,
+                FileFormat=self.format[_format]
+            )
+            return self.read(path)
+        else:
+            self.doc.Close(SaveChanges=True)
+            return self.read(self.path)
 
     def read(self, path):
         with open(path) as f:
@@ -102,13 +87,23 @@ class Tempdoc():
     def __exit__(self, exc_type, exc_val, exc_tb):
         try:
             self.doc.Close()
-            os.remove(self.path)
+            for path in self.temp_files:
+                os.remove(path)
         except:
             pass
 
         if not self.word_given:
             self.word.Quit()
 
+
+def findPath(file_name):
+    script_dir = os.path.dirname(__file__)  # absolute dir the script is in
+    rel_path = f"Safety Programs/{file_name}"
+    abs_file_path = os.path.join(script_dir, rel_path)
+    return abs_file_path
+
+
+# turn a document into byte string
 def DocumentBytes(doc):
     file_stream = io.BytesIO()
     doc.save(file_stream)
@@ -116,6 +111,19 @@ def DocumentBytes(doc):
     return file_stream.getvalue()
 
 
+# Zip files
+def zip_files(files):
+    mem_zip = io.BytesIO()
+    with ZipFile(mem_zip, mode="w") as zf:
+        for file in files:
+            zf.writestr(file[0], file[1])
+    zf.close()
+    return mem_zip
+
+
+# Hack to merge files.
+# docxtpl only lets you put 1 document at any given jinja marker.
+# We merge they subdocuments by just stealing the XML, then concat the XML and attach it to this in this dummy document.
 class DummyDoc(Subdoc):
     def __init__(self, tpl, xml):
         super().__init__(tpl)  # tpl is main document, it is normally passed to subdoc class
@@ -134,6 +142,7 @@ def create_manual(
     # Create subdoc to insert into main document
     main_document.init_docx()  # Not sure what the point of this is but lib normally does it before init Subdoc.
 
+    # merge many docs into maindoc
     xml = ""
     compose = SubdocComposer(main_document)
     for doc in safety_documents:
@@ -162,23 +171,26 @@ def create_manual(
         b = td.save()
     return b
 
+
 def create_program(
         files: list,  # TODO Should maybe be bytes
         company_name: str
 ):
     docs = []
-    wd = Tempdoc.word_instance() # slow to reopen and close word repeatedly
+    wd = Tempdoc.word_instance()  # slow to reopen and close word repeatedly
     for file in files:
         main_document = DocxTemplate(file)
 
+        # add company name
         ctx = {
             "company_name": company_name
         }
-
         main_document.render(ctx)
 
-        # docx
         data = DocumentBytes(main_document)
+        # data to be appended
+
+        # append docx to zip
         docs.append(
             [
                 os.path.basename(file),
@@ -186,9 +198,10 @@ def create_program(
             ],
         )
 
-        # pdf
+        # produce a pdf from data bytes
         with Tempdoc(data) as td:
-            data_pdf = td.save_as("pdf")
+            data_pdf = td.save("pdf")
+        # append to zip
         docs.append(
             [
                 os.path.basename(file)[:-4] + "pdf",
@@ -196,18 +209,4 @@ def create_program(
             ],
         )
 
-            
     return zip_files(docs).getvalue()
-
-def zip_files(files):
-    mem_zip = io.BytesIO()
-    with ZipFile(mem_zip, mode="w") as zf:
-        for file in files:
-            zf.writestr(file[0], file[1])
-    zf.close()
-    return mem_zip
-
-# create_manual(findPath("safety_manual.docx"), [findPath("aerial lifts.docx"), findPath("cranes.docx"), findPath("cadmium.docx")], "Test Name LLC.")
-# update_toc('Output/new_safety_manual.docx')
-
-# create_program(findPath("aerial lifts.docx"), "Test Name.")
